d3b77dce960a4c91aa811fddfa8c8e0b684f324c
AR8Stefan <stefan.piatkov@gmail.com>
2019-07-07 17:23:26 -0500

    Understanding how to quickly build an App using React.JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results'
+
+import {
+  Container,
+  Jumbotron
+} from 'react-bootstrap';
 
 function App() {
   return (
     <div>
+      <Jumbotron>
+        <Container>
+          <h1>Test App</h1>
+          <p>This is just an app</p>
 
+          <SearchForm />
+        </Container>
+      </Jumbotron>
+      <Results />
     </div>
   );
 }
